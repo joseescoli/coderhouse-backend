0240c338a36b3d956813dfebb839984089c3a7ef
joseescoli <jose.a.escoli@gmail.com>
2023-06-14 08:14:48 -0300

    Entrega2

--- Entrega2/ProductManager.js
+++ Entrega2/ProductManager.js
@@ -22,19 +22,21 @@ class ProductManager {
         return !!prod.title && !!prod.description && !!prod.price && !!prod.code && !!prod.thumbnail && !!prod.stock
     }
 
-    #duplicateCode ( prod ) {
-        return !this.products.some( p => p.code === prod.code)
+    #duplicateCode ( code ) {
+        return this.products.some( p => p.code !== code)
     }
     
     
     // Método para obtener los productos
-    async getProducts () {
+    getProducts () {
         try {
             if (fs.existsSync(this.path)){
-                const products = await fs.promises.readFile(this.path, 'utf8');
-                this.products.push(JSON.parse(products))
+                const products = fs.readFileSync(this.path, 'utf8');
+                this.products = [...JSON.parse(products)]
+                // console.log(this.products);
                 return this.products
             } else {
+                // console.log(this.products);
                 return this.products
             }
         } catch (error) {
@@ -43,14 +45,13 @@ class ProductManager {
     }
 
     // Método para guardar los productos cargados
-    async #saveProducts () {
+    #saveProducts () {
         try {
-            await fs.promises.writeFile(this.path, JSON.stringify(this.products));
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
         } catch (error) {
             console.log(error);
         }
     }
-    
     // Método para cargar productos enviado como objeto
     addProduct(prod) {
     
@@ -60,7 +61,7 @@ class ProductManager {
             console.log(error)
         }
 
-        if ( this.#duplicateCode( prod ) ) {
+        if ( this.#duplicateCode (prod.code) || this.products.length === 0 ) {
             if ( this.#allInfoCheck( prod ) ) {
                 this.#id = this.#lastId() + 1
 
@@ -68,9 +69,11 @@ class ProductManager {
                 this.products.push( producto )
                 this.#saveProducts()
             } else {
+                console.log('One or several attributes do not have proper information. Please, verify!');
                 return 'One or several attributes do not have proper information. Please, verify!'
             }
         } else {
+            console.log('Duplicate "code" attribute! Please, choose another!');
             return 'Duplicate "code" attribute! Please, choose another!'
         }
     }
@@ -93,16 +96,15 @@ class ProductManager {
     }
 
     // Método para actualizar un producto
-    updateProduct ( obj ) {
+    async updateProduct ( obj ) {
 
         try {   this.getProducts()  }
         catch (error) { console.log(error)  }
 
         if ( this.products.some( p => p.id === obj.id) ) {
+            const index = this.products.findIndex( p => p.id === obj.id )
 
-            if ( this.#duplicateCode( obj ) ) {
-
-                const index = this.products.findIndex( p => p.id === obj.id )
+            if ( this.#duplicateCode( obj.code ) || this.#duplicateCode( this.products[index].code ) ) {
 
                 if ( index >= 0 ) {
                     const oldProd = this.products[index]
@@ -125,7 +127,7 @@ class ProductManager {
     }
 
     // Método para eliminar un producto mediante su ID
-    deleteProduct (ID) {
+    async deleteProduct (ID) {
 
         try {
             this.getProducts()
@@ -146,23 +148,27 @@ class ProductManager {
   
 }
 
-
+/*
+(
+    async () => {
+  */  
 
 // TESTING propuesto en documento "Clase 2 - Testing de Entregable"
 console.log("TESTING propuesto en documento -- Testing de Entregable\n\n");
 
 // Instancia 1 de clase definida
-const instancia1 = new ProductManager('./products.json')
+const instancia1 = new ProductManager('./Entrega2/products.json')
 
 console.log(`Listado de productos. Vacío o recuperando los productos del archivo.
 ======================================================`)
-instancia1.getProducts()
+console.log( instancia1.getProducts() );
 console.log(`======================================================`);
 
 // Carga de producto
 console.log("\nCarga de producto\n======================================================");
+
 instancia1.addProduct(
-{
+    {
     title: "Producto Prueba",
     description: "Este es un producto de prueba",
     price: 200,
@@ -173,21 +179,22 @@ instancia1.addProduct(
 )
 console.log("======================================================\n");
 
+
 // Listado de productos. Para este caso un único producto.
 console.log(`Listado de productos. Único producto o todos incluyendo el último recuperando los productos del archivo.
 ======================================================`);
-instancia1.getProducts()
+console.log( instancia1.getProducts() );
 console.log(`======================================================`);
 
 
 // Consulta de producto por ID inexistente
 console.log("\nConsulta por ID inexistente\n======================================================");
-instancia1.getProductById(3)
+console.log( instancia1.getProductById(10) );
 console.log("======================================================\n");
 
 // Consulta de producto por ID existente
 console.log("\nConsulta por ID Existente\n======================================================");
-instancia1.getProductById(1)
+console.log( instancia1.getProductById(1) );
 console.log("======================================================\n");
 
 
@@ -206,24 +213,24 @@ console.log(`======================================================`);
 
 console.log(`Listado de productos. Ver producto cambiado
 ======================================================`);
-instancia1.getProducts()
+console.log( instancia1.getProducts() );
 console.log(`======================================================`);
 
 console.log(`Borrar producto ID 2
 ======================================================`);
-instancia1.deleteProduct(2)
+console.log( instancia1.deleteProduct(2) );
 
 console.log(`======================================================`);
 
 console.log(`Listado de productos. Ver producto borrado
 ======================================================`);
-instancia1.getProducts()
+console.log( instancia1.getProducts() );
 console.log(`======================================================`);
 
 
 // TESTING ADICIONAL - Segunda instancia de la clase
 console.log("\n\nTESTING ADICIONAL\n\n");
-const instancia2 = new ProductManager('./products.json')
+const instancia2 = new ProductManager('./Entrega2/products.json')
 
 
 console.log("\nCarga producto repetido\n======================================================");
@@ -239,18 +246,9 @@ instancia2.addProduct(
         stock: 25
     }
     )
-
-instancia2.addProduct(
-        {
-            title: "Producto Prueba",
-            description: "Este es un producto de prueba",
-            price: 200,
-            thumbnail: "Sin imagen",
-            code: "abc123",
-            stock: 25
-        }
-        )
     
 console.log("======================================================\n");
 
-instancia2.getProducts()
+console.log( instancia2.getProducts() );
+
+//})()
